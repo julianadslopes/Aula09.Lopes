31ef6bcd885d4fc34c0fe8dea20db315b772ac29
julianadslopes <jujulianalopes@live.com>
2024-10-08 20:47:14 -0300

    exemplo try - informando tipo de erro

--- Aula09/exemplo1.py
+++ Aula09/exemplo1.py
@@ -1,9 +1,12 @@
 import os
 os.system ("cls")
 
+# try:
+#     n = int(input("Informe um número: "))
+# except:
+#     print ("Erro!")
+
 try:
     n = int(input("Informe um número: "))
-except:
-    print ("Erro!")
-
-
+except (ValueError, KeyboardInterrupt) as e:  # e vai ser usado como apelido para o erro do tipo de valor 
+    print (f"{e}")
