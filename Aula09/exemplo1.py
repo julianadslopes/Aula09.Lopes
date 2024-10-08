3449ecbb368e23ddaec3a972ecb8d40bf98fd793
julianadslopes <jujulianalopes@live.com>
2024-10-08 20:56:44 -0300

    exemplo try-except-else

--- Aula09/exemplo1.py
+++ Aula09/exemplo1.py
@@ -6,7 +6,15 @@ os.system ("cls")
 # except:
 #     print ("Erro!")
 
+# try:
+#     n = int(input("Informe um número: "))
+# except (ValueError, KeyboardInterrupt) as e:  # e vai ser usado como apelido para o erro do tipo de valor 
+#     print (f"{e}")
+
+
 try:
-    n = int(input("Informe um número: "))
-except (ValueError, KeyboardInterrupt) as e:  # e vai ser usado como apelido para o erro do tipo de valor 
-    print (f"{e}")
+    txt = input ("Informe o nome: ") [0] # o [0] indica que quero uma 'lista'
+except IndexError as e:
+    print (f" {e} Precisa digitar algo")
+else: # Neste caso, o else só vai ser rodado se NÃO DER ERRO
+    print ("Acertou!")    
