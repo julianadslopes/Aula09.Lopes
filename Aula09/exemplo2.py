b3af5e1c7c205bbfb0f4a99ca4fc7dffaecfeb24
julianadslopes <jujulianalopes@live.com>
2024-10-08 21:25:35 -0300

    exemplo try - except com raise

--- Aula09/exemplo2.py
+++ Aula09/exemplo2.py
@@ -5,11 +5,10 @@ try:
     resp = input ("Informe (s/n): ") .lower()
 
     if resp == "":
-        raise EOFError ("Você não digitou nada")
+        raise EOFError ("Você não digitou nada") # o raise gera uma exceção manualmente
     if resp.isdigit (): # .isdigit verifica se é dígito
         raise ValueError ("Não informe números")
 except EOFError as e:
     print (f"{e}")
 except ValueError as e:
     print (f"{e}")
-            
